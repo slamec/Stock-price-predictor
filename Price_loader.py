bcc6d6da5eb4baa2221af7296c6e944cf562c5ce
Miroslav Kopecky <kopecky.mir@gmail.com>
2021-02-01 21:19:02 +0100

    load price and save to file

--- Price_loader.py
+++ Price_loader.py
@@ -2,3 +2,13 @@ import pandas as pd
 import datetime
 import pandas_datareader.data as web
 from pandas import Series, DataFrame
+import sys
+
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2020, 12, 31)
+
+stock_data = web.DataReader("AAPL", 'yahoo', start, end)
+stock_data.tail()
+
+with open('filename.csv', 'w') as f:
+    print(stock_data, file=f)
