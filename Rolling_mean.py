b7874efea3ce2ef6ef915d9af09d89dfd0e296dd
Miroslav Kopecky <kopecky.mir@gmail.com>
2021-02-02 22:08:45 +0100

    import of matplotlib

--- Rolling_mean.py
+++ Rolling_mean.py
@@ -4,7 +4,15 @@ import pandas_datareader.data as web
 from pandas import Series, DataFrame
 import sys
 
+import matplotlib.pyplot as plt #importing matplotlib 
+import matplotlib as mpl
+from matplotlib import style
+
+
 pd.set_option('display.max_rows', None) #show all the rows "None" is no limit
+plt.rcParams["figure.figsize"] = (8,7) #adjusting size of matblotlib
+style.use('ggplot')
+
 
 start = datetime.datetime(2015, 1, 1)
 end = datetime.datetime(2020, 12, 31)
